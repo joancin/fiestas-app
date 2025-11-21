86dd9e511a5aae821b2bedb9aa7c1305871d0693
joancin <joan64rl@gmail.com>
2025-11-21 16:57:26 +0100

    Add files via upload
    gravedad

--- main.tsx
+++ main.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App';
-import { HashRouter } from 'react-router-dom';
-import './index.css';
+import App from './App';
+import './src/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -11,7 +10,5 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <HashRouter>
     <App />
-  </HashRouter>
 )
