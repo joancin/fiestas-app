02e2c8c50b493201f78a806d9a507b231adf074c
joancin <joan64rl@gmail.com>
2025-11-21 17:12:38 +0100

    Fix launch issues and add deploy workflow

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../App';
-import './index.css';
+import App from './App';
+import './src/index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,7 +10,5 @@ if (!rootElement) {
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+)
