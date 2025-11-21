0adeb8168a162660db280b9194d34084554e0a2c
joancin <joan64rl@gmail.com>
2025-11-21 17:11:45 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fiestas-app/',
+  base: './',
   build: {
     rollupOptions: {
       output: {
